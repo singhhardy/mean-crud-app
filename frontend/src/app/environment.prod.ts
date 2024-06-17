236733437650e6a7b76133b07a744476ace47785
hardikWottaweb <hardik.wottaweb@gmail.com>
2024-06-17 10:35:20 +0530

    prod

---
+++ frontend/src/app/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://https://mean-crud-app-fde6.onrender.com/'
+}
--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  private apiUrl = 'http://localhost:5000/api/users'
+  private apiUrl = `${environment.apiUrl}/api/users`
 
   constructor(private http: HttpClient) { }
 
